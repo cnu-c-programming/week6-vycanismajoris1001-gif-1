a8113cb1667797677cf4d769d203f6c56edbd4b4
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-04-10 10:33:52 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -5,7 +5,7 @@ int main() {
 
   for (int i=0; i<5; i++) {
       for (int j=0; ; j++) {
-         char c = *(names+i);
+         char c = *(names+j);
       if (c== '\0')
         break;
       printf("%c", c);
