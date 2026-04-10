db16588cbfda47993351c0a38fb1a39b445f6ec3
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-04-10 13:28:05 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -3,7 +3,7 @@
 void print_2d_arr(int* element, int row_size, int col_size) {
   for (int i=0; i<row_size; i++) {
      for (int j=0; j<col_size; j++) {
-          printf("%d ", *element[i][j]);
+          printf("%d ", *(element + (i * col_size) + j);
      }
    printf("\n");
   }
